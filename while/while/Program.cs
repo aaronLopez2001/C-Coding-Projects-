83ff01140f4dc0c0aa1c5e9186f66b5127a06cd5
aaronLopez2001 <105614197+aaronLopez2001@users.noreply.github.com>
2022-10-01 15:59:28 -0700

    Add files via upload

---
+++ while/while/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is 5 * 5 ?");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            bool correct = answer == 25;
+            while (!correct)
+            {
+                switch (answer)
+                {
+                    case 25:
+                        Console.WriteLine("That is correct!");
+                        Console.Read();
+                        break;
+                    case 35:
+                        Console.WriteLine($"{answer} is incorrect please try again!");
+                        answer = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 41:
+                        Console.WriteLine($"{answer} is incorrect please try again!");
+                        answer = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine($"{answer} is incorrect please try again!");
+                        answer = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine($"{answer} is incorrect please try again!");
+                        answer = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+            }
+            Console.Read();
+
+        }
+    }
+}

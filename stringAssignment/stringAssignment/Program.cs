7f3e557d104fae66770f82f009e1b4f17e457ba0
aaronLopez2001 <105614197+aaronLopez2001@users.noreply.github.com>
2022-10-01 17:24:54 -0700

    Add files via upload

---
+++ stringAssignment/stringAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Where are you from?");
+            string address = Console.ReadLine();
+            string firstString = "Hello ";
+            string secondString = "My name Is ";
+            string thirdString = " and I am from ";
+            Console.WriteLine($"{firstString}{secondString}{name}{thirdString}{address}");
+            Console.WriteLine(name.ToUpper());
+           
+            StringBuilder lbrry = new StringBuilder();
+            lbrry.Append($"My name Is {name}");
+            Console.WriteLine(lbrry);
+            Console.Read();
+        }
+    }
+}

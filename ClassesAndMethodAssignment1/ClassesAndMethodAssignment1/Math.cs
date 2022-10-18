9c3123433b81543708790664e7cb573701c9c917
aaronLopez2001 <105614197+aaronLopez2001@users.noreply.github.com>
2022-10-18 14:19:44 -0700

    Add files via upload

---
+++ ClassesAndMethodAssignment1/ClassesAndMethodAssignment1/Math.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodAssignment1
+{
+    public class Math
+    {
+        //First method takes in an Integer
+        public int Arithmetic(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        //Second method that takes in decimal
+        public decimal Arithmetic(decimal num1, decimal num2)
+        {
+            int number = Convert.ToInt32(num1);
+            int number2 = Convert.ToInt32(num2);
+            return number * number2;
+        }
+        public string Arithmetic(string num1, string num2)
+        {
+            Convert.ToInt32(num1);
+            Convert.ToInt32(num2);
+
+            return num1 + num2 ;
+
+        }
+
+    }
+
+
+}

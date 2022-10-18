09513ce3fc641bdba24c1a046c12056cc7b1a69b
aaronLopez2001 <105614197+aaronLopez2001@users.noreply.github.com>
2022-10-18 13:18:32 -0700

    Add files via upload

---
+++ ClassesAndMethodAssignment1/ClassesAndMethodAssignment1/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodAssignment1
+{
+    public class Math
+    {
+        //First method takes in an Integer
+        public int Arithmetic(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        //Second method that takes in decimal
+        public decimal Arithmetic(decimal num1, decimal num2)
+        {
+            return num1 * num2;
+        }
+        public string Arithmetic(string num1, string num2)
+        {
+            int.Parse(num1);
+            int.Parse(num2);
+            return num1+ num2;
+
+        }
+
+    }
+
+
+}

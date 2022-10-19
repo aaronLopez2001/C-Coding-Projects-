4e42b994e5f77e2b6c396e5317124027309b19bc
aaronLopez2001 <105614197+aaronLopez2001@users.noreply.github.com>
2022-10-19 10:43:48 -0700

    Add files via upload

---
+++ ClassesAndMethodAssignment2/ClassesAndMethodAssignment2/Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodAssignment2
+{
+    class Class
+    {
+        public int Math(int x, int y = 10)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ ClassesAndMethodAssignment2/ClassesAndMethodAssignment2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Class arithmetic = new Class();
+           
+            Console.WriteLine("Type in a number!");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type in a second number!(This one is optional)");
+            string num2 = Console.ReadLine();
+            
+            if (num2 == "")
+            {
+                Console.WriteLine(arithmetic.Math(num1));
+            }
+            else
+            {
+                int number2 = Convert.ToInt32(num2);
+                Console.WriteLine(arithmetic.Math(num1,number2));
+            }
+          
+
+            
+            Console.Read();
+        }
+    }
+}

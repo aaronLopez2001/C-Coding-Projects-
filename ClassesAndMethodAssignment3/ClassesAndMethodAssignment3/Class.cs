03a61782d3f2cb00b799efe931a4829802734671
aaronLopez2001 <105614197+aaronLopez2001@users.noreply.github.com>
2022-10-19 13:35:11 -0700

    Add files via upload

---
+++ ClassesAndMethodAssignment3/ClassesAndMethodAssignment3/Class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodAssignment3
+{
+   public class Class
+    {
+        public void Practice(int num1, int num2 )
+        {
+            int result = num1 + 48;
+            Console.WriteLine("Your day of birth is: "+ num2);
+
+        }
+    }
+}
---
+++ ClassesAndMethodAssignment3/ClassesAndMethodAssignment3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class obj = new Class();
+            Console.WriteLine("How old are you?");
+            int myAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the day of your bithday?");
+            int bornDay = Convert.ToInt32(Console.ReadLine());
+
+
+            obj.Practice(myAge,bornDay);
+           
+
+           
+
+
+            Console.ReadKey();
+        }
+
+    }
+}

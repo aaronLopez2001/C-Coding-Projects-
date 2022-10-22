1adc7dd9c599372b213023e50a37e7d45443574e
aaronLopez2001 <105614197+aaronLopez2001@users.noreply.github.com>
2022-10-21 18:26:16 -0700

    Add files via upload

---
+++ ClassMethodAssignment/ClassMethodAssignment/VoidClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    class VoidClass
+    {
+
+
+        public void Output(out int x)
+        {
+            x = 0;
+            while(x != 100)
+            {
+                Console.WriteLine(x+=10);
+            }
+         
+        }
+        //Overload a method
+        public void Output(int num1, int y, out int b)
+        {
+            b = num1 * y;
+
+        }
+
+    }
+}

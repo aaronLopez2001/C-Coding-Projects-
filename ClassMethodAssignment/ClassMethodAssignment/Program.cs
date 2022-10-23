cdd644e69a868faebca3aea1eab193237fc44004
aaronLopez2001 <105614197+aaronLopez2001@users.noreply.github.com>
2022-10-23 11:57:47 -0700

    Add files via upload

---
+++ ClassMethodAssignment/ClassMethodAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            VoidClass obj = new VoidClass();
+            Console.WriteLine("Type in a number!");
+            int myNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type in another number!");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type in a third number!");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+            obj.Operation(ref myNum);
+            obj.Output(out int x);
+            obj.Output(numero1,numero2, out int b);
+            
+           
+         
+            Console.WriteLine(x);
+            Console.WriteLine($"Your number divided by {StaticClass.num1} is = " + myNum);
+            Console.WriteLine(b);
+            Console.ReadLine();
+ 
+      
+        }
+
+    }
+}
---
+++ ClassMethodAssignment/ClassMethodAssignment/VoidClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+      class VoidClass 
+    {
+      
+
+        public void Output(out int x)
+        {
+            x = 0;
+            while(x != 100)
+            {
+                Console.WriteLine(x+=10);
+            }
+         
+        }
+        //Overload a method
+        public void Output(int num2, int y, out int b)
+        {
+            b = num2 + y;
+
+        }
+      
+        public void Operation(ref int myNum)
+        {
+            myNum /= StaticClass.num1;
+
+        }
+
+
+    }
+
+    public static class StaticClass
+    {
+        public static int num1 = 25;
+    }
+}

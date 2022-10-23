1c2b3b14ad60d001687e2ed074a6479cea2522e3
aaronLopez2001 <105614197+aaronLopez2001@users.noreply.github.com>
2022-10-22 18:05:41 -0700

    Add files via upload

---
+++ InheritanceAssignment/InheritanceAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee obj = new Employee();
+            obj.firstName = "Sample";
+            obj.lastName = "Student";
+            obj.SayName();
+            Console.Read();
+
+           
+
+        }
+    }
+}

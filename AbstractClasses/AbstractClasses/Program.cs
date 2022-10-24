30e7af82864bedf0c7b682a8ce206010442dac9d
aaronLopez2001 <105614197+aaronLopez2001@users.noreply.github.com>
2022-10-23 17:59:45 -0700

    Add files via upload

---
+++ AbstractClasses/AbstractClasses/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee obj = new Employee();
+            obj.firstName = "Sample";
+            obj.lastName = "Student";
+            obj.SayName();
+            Console.ReadLine();
+        }
+    }
+}

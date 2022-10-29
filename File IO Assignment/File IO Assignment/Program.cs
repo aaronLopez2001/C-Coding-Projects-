77c1f498f9f78b409ecc910afe295ae1ce5cd492
aaronLopez2001 <105614197+aaronLopez2001@users.noreply.github.com>
2022-10-29 14:30:33 -0700

    Add files via upload

---
+++ File IO Assignment/File IO Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace File_IO_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write a number");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            string answerToString = Convert.ToString(answer);
+            string answr = string.Format(answerToString);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\azehu\OneDrive\Desktop\FileWriteAllText\assignment.txt", false))
+            {
+                file.Write(answr);
+            }
+            Console.WriteLine(answr);
+            Console.Read();
+        }
+    }
+}

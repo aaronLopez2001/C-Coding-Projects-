5e50b903cf2cf5cf9eb581e4a1dc9c01a97ebc57
aaronLopez2001 <105614197+aaronLopez2001@users.noreply.github.com>
2022-10-30 16:11:18 -0700

    Add files via upload

---
+++ Additional Fealtures Assignment/Additional Fealtures Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Fealtures_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "Aaron";
+            var age = 21;
+            Console.WriteLine("{0} is {1} years old!", name, age);
+            Console.Read();
+        }
+    }
+}

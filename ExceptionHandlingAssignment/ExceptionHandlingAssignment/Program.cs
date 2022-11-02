72b40bc7cc46e40aeebccada65e9e078e8b41a0a
aaronLopez2001 <105614197+aaronLopez2001@users.noreply.github.com>
2022-11-02 14:27:50 -0700

    Add files via upload

---
+++ ExceptionHandlingAssignment/ExceptionHandlingAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            int year = DateTime.Now.Year;
+            int yearBorn = year - age;
+
+            Console.WriteLine(yearBorn);
+            Console.Read();
+
+            
+             
+
+        }
+    }
+}

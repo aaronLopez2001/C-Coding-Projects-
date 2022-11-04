93d4a699b2d2de34d01e0d42086e61177d282dca
aaronLopez2001 <105614197+aaronLopez2001@users.noreply.github.com>
2022-11-03 19:18:56 -0700

    Add files via upload

---
+++ FinalAssignment/FinalAssignment/wwwroot/js/JavaScript.js
@@ -0,0 +1,10 @@
+﻿
+
+var currentDay = new Date();
+var weeks = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var dayOfTheWeek = weeks[currentDay.getDay()]
+
+function Click() {
+    window.alert("Today is " + dayOfTheWeek)
+}

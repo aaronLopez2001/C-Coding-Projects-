2031543a4c1bd91a1e3291a8521454cd3c512fce
aaronLopez2001 <105614197+aaronLopez2001@users.noreply.github.com>
2022-11-03 17:07:59 -0700

    Add files via upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,85 +29,12 @@ namespace CarInsurance.Controllers
 
             return View();
         }
-        public ActionResult CalculateQuote()
+
+        public ActionResult MyQuote()
         {
-            using (InsuranceEntities db = new InsuranceEntities())
-            {
-                var insurees = db.Insurees;
-                var insureeVms = new List<InsureeVm>();
-                foreach (var insuree in insurees)
-                {
-                    var insureeVm = new InsureeVm();
-                    insureeVm.FirstName = insuree.FirstName;
-                    insureeVm.LastName = insuree.LastName;
-                    insureeVm.EmailAddress = insuree.EmailAddress;
-                    insureeVm.DateOfBirth = insuree.DateOfBirth;
-                    insureeVm.CarYear = insuree.CarYear;
-                    insureeVm.CarMake = insuree.CarMake;
-                    insureeVm.CarModel = insuree.CarModel;
-                    insureeVm.CoverageType = insuree.CoverageType;
-                    
-                    
 
-                    int age = insuree.DateOfBirth.Year;
-                    int year = DateTime.Now.Year;
-                    int myAge = year - age;
-                    //Checks age
-                    if(myAge < 18)
-                    {
-                        insuree.Quote =+ 150.00m;
-                    }
-                    else if(myAge >= 19 && myAge <= 25)
-                    {
-                        insuree.Quote = +100.00m;
-                    }
-                    else
-                    {
-                        insuree.Quote += 75.00m;
-                    }
-                    //Checks the year of the car
-                    if (insuree.CarYear < 2000)
-                    {
-                        insuree.Quote += 25.00m;
-                    }
-                    else if(insuree.CarYear > 2015)
-                    {
-                        insuree.Quote += 25.00m;
-                    }
-                    if(insuree.CarMake == "porsche" || insuree.CarMake == "Porsche" || insuree.CarMake == "PORSCHE" && insuree.CarModel == "911 Carrera") 
-                    {
-                        insuree.Quote += 25.00m;
-                    }
-                    if(insuree.SpeedingTickets > 0)
-                    {
-                        insuree.Quote = insuree.SpeedingTickets * 10.00m;
-                    }
-                    if(insuree.DUI == true)
-                    {
-                        insuree.Quote = insuree.Quote * 0.25m;
-                    }
-                    if (insuree.CoverageType == true)
-                    {
-                        insuree.Quote = insuree.Quote * 0.50m;
-                    }
-
-                    decimal totalQuote = insuree.Quote;
-
-                    insureeVm.Quote = totalQuote;
-                    insureeVms.Add(insureeVm);
-
-
-                    
-                   
-                }
-            }
-            
-
-                return RedirectToAction("insuree");
-
-
+            return View();
         }
-
 
 
     }
